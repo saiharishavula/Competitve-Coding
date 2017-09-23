cd0c785dce764cff45685b06720e1f2d92a3f867
Sai Harish Avula <harish02061995@gmail.com>
2017-09-23 19:12:25 +0530

    Update MARTIAN.cpp

--- MARTIAN.cpp
+++ MARTIAN.cpp
@@ -8,33 +8,38 @@ int sum_b[502][502];
 int main(){
 int n,m;
 while(1){
-
+  
   cin>>n>>m;
   if(n==0 && m==0)
   break;
-
+  // taking input
   for(int i=1;i<=n;i++)
   for(int j=1;j<=m;j++)
   cin>>b[i][j];
   for(int i=1;i<=n;i++)
   for(int j=1;j<=m;j++)
   cin>>a[i][j];
+  
+  // base case for dp
   for(int i=0;i<=m;i++)
   dp[0][i]=0,sum_a[0][i]=0;
   for(int i=0;i<=n;i++)
   dp[i][0]=0,sum_b[i][0]=0;
 
+  //precalculation
   for(int i=1;i<=n;i++)
   for(int j=1;j<=m;j++)
   sum_a[i][j] = sum_a[i-1][j]+a[i][j];
   for(int i=1;i<=n;i++)
   for(int j=1;j<=m;j++)
   sum_b[i][j] = sum_b[i][j-1]+b[i][j];
-
+ 
+  // main dp formula
   for(int i=1;i<=n;i++)
-  for(int j=1;j<=m;j++){
-    dp[i][j] = max(dp[i][j-1]+sum_a[i][j],dp[i-1][j]+sum_b[i][j]);
-  }
+  for(int j=1;j<=m;j++)
+  dp[i][j] = max(dp[i][j-1]+sum_a[i][j],dp[i-1][j]+sum_b[i][j]);
+  
+  // answer
   cout<<dp[n][m]<<endl;
 }
 }
